9a709f7b039efe8eea951a81259c5f4568faa9ce
Kris Osmakov <krisosmakov@MacBook-Air-Kris.local>
2025-05-10 00:46:49 +0300

    fix: fixed

--- index.js
+++ index.js
@@ -36,83 +36,60 @@ bot.api.setMyCommands([
     { command: 'help', description: 'Помощь' },
 ]);
 
+
+// Обработка кнопки "Автозапчасти"
+// bot.hears('Автозапчасти', async (ctx) => {
+//     await ctx.reply(
+//         'Заполните данные об автомобиле:\n' + 
+//         '(Все данные вводить через точку с запятой (";"). \n' +
+//         'В примечании стоит указать, в какой стране — Китае или ОАЭ — предпочтительнее искать запчасти).\n' +
+//         '\n' +
+//         'Марка\n' + 
+//         'Модель\n' + 
+//         'Год (от 2016 г.в.)\n' + 
+//         'VIN - номер\n' + 
+//         'Запасная часть\n' + 
+//         'Примечание\n' +
+//         '\n' + 
+//         'Пример ввода:  Mazda; CX-5; 2018; ABC123456789; Крышка багажника, капот, крыло переднее правое; Нужны оригинальные запчасти. \n'
+//     );
+//     ctx.session.waitingForCarData = true; // Устанавливаем флаг ожидания данных
+// });
+
+// bot.hears('Автозапчасти Китай', async (ctx) => {
+//     await ctx.reply(
+//         'Заполните данные об автомобиле:\n' + 
+//         '(Все данные вводить через точку с запятой (";"), пункты со "*" обязательны для заполнения) \n' +
+//         '\n' +
+//         'Марка*\n' + 
+//         'Модель*\n' + 
+//         'Год (от 2016 г.в.)*\n' + 
+//         'VIN - номер*\n' + 
+//         'Запасная часть*\n' + 
+//         'Примечание\n' +
+//         '\n' + 
+//         'Пример ввода:  Mazda; CX-5; 2018; ABC123456789; Крышка багажника, капот, крыло переднее правое; Нужны оригинальные запчасти. \n'
+//     );
+//     ctx.session.waitingForCarData = true; // Устанавливаем флаг ожидания данных
+//  });
+
 // Обработка кнопки "Заказы с POIZON"
-bot.hears('Заказы с POIZON', async (ctx) => {
+bot.hears('Заказы обуви с POIZON', async (ctx) => {
     ctx.session.waitingForPrice = true; // Устанавливаем флаг ожидания цены
-    await ctx.reply('Укажите стоимость товара в юанях (¥), чтобы я рассчитал стоимость выкупа в USD.');
+    await ctx.reply('Укажите стоимость обуви в юанях (¥), чтобы я рассчитал стоимость в USD с доставкой в Минск.');
 });
+
+bot.hears('Заказы иных товаров с POIZON', async (ctx) => {
+    ctx.session.waitingForOtherPrice = true; // Устанавливаем флаг ожидания цены
+    await ctx.reply('Укажите стоимость товара в юанях (¥), чтобы я рассчитал стоимость в USD с доставкой в Минск.');
+});
+
 
 // Обработка текстовых сообщений
 bot.on('message:text', async (ctx) => {
-//     if (ctx.session.waitingForCarData) {
-//         // Обработка данных об автомобиле
-//         const userInput = ctx.message.text;
-
-//         // Разбиваем введенные данные по точке с запятой
-//         const dataParts = userInput.split(';').map(part => part.trim());
-
-//         // Проверяем, что введены все 6 частей данных
-//         if (dataParts.length === 6) {
-//             const [brand, model, year, vin, part, note] = dataParts;
-
-//             // Проверяем, что год является числом
-//             const yearNumber = parseInt(year, 10);
-//             if (isNaN(yearNumber)) {
-//                 await ctx.reply('Год выпуска должен быть числом. Пожалуйста, исправьте данные.');
-//                 return;
-//             }
-
-//             // Проверяем, что год >= 2016
-//             if (yearNumber < 2016) {
-//                 await ctx.reply('К сожалению, мы не подбираем запчасти для автомобилей старше 2016 года выпуска.');
-//                 return;
-//             }
-//             if (vin.length < 17) {
-//                 await ctx.reply('VIN - номер должен содержать 17 символов. Пожалуйста, исправьте данные.');
-//                 return;
-//             }
-
-//             // Форматируем данные для вывода
-//             const formattedData = `
-// Марка: ${brand}
-// Модель: ${model}
-// Год (от 2016 г.в.): ${year}
-// VIN - номер: ${vin}
-// Запасная часть: ${part}
-// Примечание: ${note}
-//             `;
-
-//             // Отправляем отформатированные данные
-//             await ctx.reply(`Данные приняты:\n${formattedData}`);
-//         } else {
-//             // Если данные введены некорректно
-//             await ctx.reply('Данные введены некорректно. Пожалуйста, используйте формат, как в примере.');
-//         }
-
-//         // Сбрасываем флаг ожидания
-//         ctx.session.waitingForCarData = false;
-//     } else if (ctx.session.waitingForPrice) {
-//         // Обработка стоимости товара
-//         const price = parseFloat(ctx.message.text);
-//         if (!isNaN(price)) {
-//             let result = price / 7;
-//             if (result < 100) {
-//                 result = result + 10;
-//             } else if (result > 100 && result < 1000) {
-//                 result = result * 1.1;
-//             } else if (result >= 1000) {
-//                 result = result * 1.05;
-//             }
-//             await ctx.reply(`
-//                 \n
-//                 Стоимость Вашего товара: ${result.toFixed(2)}USD (без учета доставки).\n\nДля оформления заказа можно обратиться к нашему специалисту - @tovarbel_by\n\nОбращаем Ваше внимание, что стоимость товара может измениться на день оплаты.
-//                 `);
-//         } else {
-//             await ctx.reply('Пожалуйста, введите стоимость в юанях (¥) (числовое значение)');
-//         }
-//         // ctx.session.waitingForPrice = false; // Сбрасываем флаг
-//     }
-const price = parseFloat(ctx.message.text);
+    if (ctx.session.waitingForOtherPrice) {
+        // Обработка стоимости обуви
+        const price = parseFloat(ctx.message.text);
         if (!isNaN(price)) {
             let result = price / 7;
             if (result < 100) {
@@ -124,11 +101,35 @@ const price = parseFloat(ctx.message.text);
             }
             await ctx.reply(`
                 \n
-                Стоимость Вашего товара: ${result.toFixed(2)}USD (без учета доставки).\n\nДля оформления заказа можно обратиться к нашему специалисту - @tovarbel_by\n\nОбращаем Ваше внимание, что стоимость товара может измениться на день оплаты.
+                Стоимость Вашего товара (без учета доставки в Минск): ${result.toFixed(2)}USD.\n\nДля оформления заказа можно обратиться к нашему специалисту - @tovarbel_by\n\nОбращаем Ваше внимание, что стоимость товара может измениться на день оплаты.
                 `);
         } else {
             await ctx.reply('Пожалуйста, введите стоимость в юанях (¥) (числовое значение)');
         }
+
+        // Сбрасываем флаг ожидания
+        ctx.session.waitingForOtherPrice = false;
+    } else if (ctx.session.waitingForPrice) {
+        // Обработка стоимости обуви
+        const price = parseFloat(ctx.message.text);
+        if (!isNaN(price)) {
+            let result = price / 7;
+            if (result < 100) {
+                result = result + 10 + 15;
+            } else if (result > 100 && result < 1000) {
+                result = result * 1.1 + 15;
+            } else if (result >= 1000) {
+                result = result * 1.05 + 15;
+            }
+            await ctx.reply(`
+                \n
+                Стоимость Вашего товара c учетом доставки в Минск: ${result.toFixed(2)}USD.\n\nДля оформления заказа можно обратиться к нашему специалисту - @tovarbel_by\n\nОбращаем Ваше внимание, что стоимость товара может измениться на день оплаты.
+                `);
+        } else {
+            await ctx.reply('Пожалуйста, введите стоимость в юанях (¥) (числовое значение)');
+        }
+        // ctx.session.waitingForPrice = false; // Сбрасываем флаг
+    }
 });
 
 // Обработка ошибок
